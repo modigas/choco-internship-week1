1492cc224d308c5d1db825a86dd4dd1eb3041363
Sagi <ssagimbayev@nu.edu.kz>
2021-01-16 04:17:04 +0600

    restructured project

--- products/tasks.py
+++
@@ -1,5 +0,0 @@
-from scraper_project.celery import app
-
-# @app.task
-# def scrape():
-#     pass
--- scraper_project/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'productscraper.settings')
-
-app = Celery('send_email')
-app.config_from_object('django_config:settings', namespace='CELERY')
-app.autodiscover_tasks()
